6741e1bab1fd8b8499d70f676eaeab8c85413af9
Tassos Manolis <fotia2007@gmail.com>
2025-12-16 15:58:26 +0100

    Working Stable
    30 minutes Refresh

---
+++ custom_components/jf_daily_cartoon/const.py
@@ -0,0 +1,1 @@
+DOMAIN = "jf_daily_cartoon"
--- custom_components/jf_daily_cartoon/sensor.py
+++ custom_components/jf_daily_cartoon/sensor.py
@@ -1,89 +1,51 @@
 
 import logging
-import voluptuous as vol
-from datetime import timedelta
 import requests
-
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
+from datetime import datetime, timedelta
+from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "JF Daily Cartoon"
-SCAN_INTERVAL = timedelta(minutes=60)
-# API Endpoint
-API_URL = "https://jahoo.gr/jf/api.php?date=today"
-# Viewer Endpoint
+SCAN_INTERVAL = timedelta(minutes=15)
+API_BASE = "https://jahoo.gr/jf/api.php"
 VIEWER_BASE_URL = "https://jahoo.gr/jf/?mode=viewer"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-})
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    async_add_entities([JFCartoonSensor()], True)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the sensor platform."""
-    name = config.get(CONF_NAME)
-    add_entities([JFCartoonSensor(name)], True)
+class JFCartoonSensor(SensorEntity):
+    def __init__(self):
+        self._attr_name = "JF Daily Cartoon"
+        self._attr_unique_id = "jf_daily_cartoon_sensor"
+        self._attr_native_value = "Initializing"
+        self._attr_extra_state_attributes = {}
 
-class JFCartoonSensor(Entity):
-    """Representation of a Sensor."""
-
-    def __init__(self, name):
-        """Initialize the sensor."""
-        self._name = name
-        self._state = None
-        self._attributes = {}
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return self._attributes
-    
     @property
     def icon(self):
-        """Icon to use in the frontend."""
         return "mdi:emoticon-happy-outline"
 
     def update(self):
-        """Fetch new state data for the sensor."""
         try:
-            response = requests.get(API_URL, timeout=10)
+            today = datetime.now().strftime('%Y-%m-%d')
+            url = f"{API_BASE}?date={today}"
+            response = requests.get(url, timeout=10)
             if response.status_code == 200:
                 data = response.json()
                 if data and 'title' in data:
-                    self._state = data['title']
-                    
-                    # Construct Viewer URL for tap_action
+                    self._attr_native_value = data['title']
                     viewer_url = f"{VIEWER_BASE_URL}&date={data.get('date', '')}"
-                    
-                    # Get Images
                     images = data.get('images', [])
-                    main_image = images[0] if images else None
-                    
-                    self._attributes = {
+                    self._attr_extra_state_attributes = {
                         "description": data.get('description', ''),
                         "images": images,
                         "date": data.get('date', ''),
-                        "image_url": main_image,
                         "viewer_url": viewer_url
                     }
                 else:
-                    self._state = "No Cartoon Today"
-                    self._attributes = {
-                        "image_url": None,
+                    self._attr_native_value = "No Cartoon Today"
+                    self._attr_extra_state_attributes = {
                         "viewer_url": VIEWER_BASE_URL
                     }
         except Exception as e:
-            _LOGGER.error(f"Error updating JF Cartoon: {e}")
+            _LOGGER.error(f"Error updating JF Sensor: {e}")
